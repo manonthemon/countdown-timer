65e1ae9a01f68dec3cd0370817a4466d16b8eb18
Manonthemon <klimowicz.maciej@gmail.com>
2023-06-07 18:18:32 +0100

    Hooked in the fodrm

--- scripts.js
+++ scripts.js
@@ -5,10 +5,12 @@ const buttons = document.querySelectorAll("[data-time]");
 
 
 function timer(seconds) {
+  //clear timer
+  clearInterval(countdown);
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
-  displayEndTime(then)
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -26,21 +28,30 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-  const display = `${minutes}:${remainderSeconds < 10 ? '0':''}${remainderSeconds}`
-  document.title = display
-  timerDisplay.textContent = display
+  const display = `${minutes}:${
+    remainderSeconds < 10 ? "0" : ""}${remainderSeconds}`;
+  document.title = display;
+  timerDisplay.textContent = display;
 }
 
 function displayEndTime(timestamp) {
-const end = new Date(timestamp)
-const hours = end.getHours()
-const minutes = end.getMinutes()
-endTime.textContent = `Be Back At ${hours}:${minutes}`
+  const end = new Date(timestamp);
+  const hours = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+
 }
 
 function startTimer() {
-    const seconds = parseInt(this.dataset.time)
-    console.log(seconds)
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
 }
 
-buttons.forEach(button => button.addEventListener('click', startTimer ))
+buttons.forEach((button) => button.addEventListener("click", startTimer));
+document.customForm.addEventListener('submit', function(e) {
+e.preventDefault();
+const mins = this.minutes.value;
+timer(mins * 60)
+this.reset();
+})
+
