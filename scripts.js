16474e0d398dfc918c2c6073cdb6162beaed8c5a
Manonthemon <klimowicz.maciej@gmail.com>
2023-06-07 18:00:05 +0100

    Displayed timer on page

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then)
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -22,7 +24,14 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-  const display = `${minutes} : ${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`
+  const display = `${minutes}:${remainderSeconds < 10 ? '0':''}${remainderSeconds}`
   document.title = display
   timerDisplay.textContent = display
+}
+
+function displayEndTime(timestamp) {
+const end = new Date(timestamp)
+const hours = end.getHours()
+const minutes = end.getMinutes()
+endTime.textContent = `Be Back At ${hours}:${minutes}`
 }
