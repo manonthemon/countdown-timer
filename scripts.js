b0102030ece4ab6b174db1e01164da3b45e072cb
Manonthemon <klimowicz.maciej@gmail.com>
2023-06-07 14:22:43 +0100

    Displayed conter on page

--- scripts.js
+++ scripts.js
@@ -1,18 +1,28 @@
 let countdown;
+const timerDisplay = document.querySelector(".display__time-left");
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
-  console.log({ now, then });
+  displayTimeLeft(seconds);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
-
-    if (secondsLeft < 0) {
+    //check if timer needs to be stopped
+    if (secondsLeft <= 0) {
       clearInterval(countdown);
       return;
     }
 
-    console.log(secondsLeft);
+    //display timer
+    displayTimeLeft(secondsLeft);
   }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes} : ${remainderSeconds}`
+  timerDisplay.textContent = display
+  console.log(minutes, remainderSeconds);
 }
